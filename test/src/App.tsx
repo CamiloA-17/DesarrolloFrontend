ee7861d4fe256cb302e66d14e6cb8546812c8a54
Camilo Andres Molano Aristizabal <camiangi1517@gmail.com>
2024-08-23 17:46:52 -0500

    class work

---
+++ test/src/App.tsx
@@ -0,0 +1,18 @@
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Principal from './components/Principal'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Principal />
+      <Footer />
+    </>
+  )
+}
+
+export default App
